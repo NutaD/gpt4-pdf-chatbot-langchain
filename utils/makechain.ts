bfb9f0f31739107fc79c0f49497c905fc31b7385
nutad <anna-detkina@yandex.ru>
2023-08-23 19:32:49 +0100

    camo init

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -11,7 +11,9 @@ Standalone question:`;
 
 const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context. 
+The information you will be dealing with is a user guide for scientific software. The user guide will be used by students.
+If the question is related to providing an example of input file based on the specific requirements, politely respond that you can not provide it at that time. 
 
 {context}
 
