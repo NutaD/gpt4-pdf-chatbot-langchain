d3cffee5a90bb0e1645f6f00c9f6b2039db43459
nutad <anna-detkina@yandex.ru>
2023-10-25 11:37:15 +0100

    gpt-4 and number of sources

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,12 +9,12 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
+
 const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context. 
 The information you will be dealing with is a user guide for scientific software. The user guide will be used by students.
-If the question is related to providing an example of input file based on the specific requirements, politely respond that you can not provide it at that time. 
-
+If the question is related to providing an example of input file based on the specific requirements, politely respond that you can not provide it at that time.
 {context}
 
 Question: {question}
@@ -23,12 +23,12 @@ Helpful answer in markdown:`;
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
     temperature: 0, // increase temepreature to get more creative answers
-    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
+    modelName: 'gpt-4', //change this to gpt-4 if you have access gpt-3.5-turbo
   });
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
     model,
-    vectorstore.asRetriever(),
+    vectorstore.asRetriever(7),
     {
       qaTemplate: QA_PROMPT,
       questionGeneratorTemplate: CONDENSE_PROMPT,
